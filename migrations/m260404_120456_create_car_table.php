47ed61b368d67e6e057f34b0328a634e7a0f727a
Ivan Yakimov <yoowhi@gmail.com>
2026-04-04 18:11:54 +0300

    car and car_option migrations

---
+++ migrations/m260404_120456_create_car_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%car}}`.
+ */
+class m260404_120456_create_car_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%car}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+            'description' => $this->text()->notNull(),
+            'price' => $this->decimal()->notNull(),
+            'photo_url' => $this->string()->notNull(),
+            'contacts' => $this->string()->notNull(),
+            'created_at' => $this->timestamp()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%car}}');
+    }
+}
